28d738e67a71696fa0b7a674150e2fc7e17758c2
MathiasB <git@denbeke.be>
2015-07-22 20:50:52 +0200

    Created altitude app

--- Altitude/ViewController.swift
+++ Altitude/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager:CLLocationManager = CLLocationManager()
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var labelAltitude: UILabel!
+    
     override func viewDidLoad() {
+        
+        activityIndicator.startAnimating()
+        
+        self.locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        self.locationManager.delegate = self
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.startUpdatingLocation()
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        let alt = newLocation.altitude
+        //print("\(alt)")
+        labelAltitude.text = String(Int(alt)) + "m"
+        manager.stopUpdatingLocation()
+        activityIndicator.stopAnimating()
+    }
+    
+    @IBAction func buttonTapToRefresh(sender: AnyObject) {
+        // Clean label & start spinner
+        self.labelAltitude.text = ""
+        self.activityIndicator.startAnimating()
+        // Start location service
+        self.locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
